84503cc12d415b3d458f81b332db3d22a2553079
Vidit Rawat <viditrawatv68@gmail.com>
2024-01-28 16:17:09 -0500

    Added services

--- client/src/service.jsx
+++ client/src/service.jsx
@@ -1,7 +1,12 @@
 export default function Service() {
     return (
       <>
-        <p>Service</p>
-      </>
+        <p>
+          - Can work on firebase.<br></br>
+          - Have a knowledge about Unity and Unreal Engine and got 2.5 years of professional experience in unity.<br></br>
+          - Have a good knowldge on DOTween, UniRx, Google play Services for Unity.<br></br>
+          </p>
+        </>
+
     );
   }
